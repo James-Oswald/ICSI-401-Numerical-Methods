11c53dac05aa55baa9f83ae5344950855f440c38
James-Oswald <jamesoswald111@gmail.com>
2020-10-01 22:12:49 -0400

    final problem

--- Homework2/hw224.m
+++ Homework2/hw224.m
@@ -1,9 +1,14 @@
 
 %Testing corectness of part 1 against an online result I found from
 %https://www.whitman.edu/mathematics/calculus_online/section06.03.html
-F1 = @(x) x^2-3;
-F1p = @(x) 2*x;
-disp(newtonsMethod(F1, F1p, 2, 2));
+%F1 = @(x) x^2-3;
+%F1p = @(x) 2*x;
+%disp(newtonsMethod(F1, F1p, 2, 2));
+
+%2.4 Part 3
+F1 = @(x) x - x*log(x);
+F1p = @(x) -log(x);
+disp(newtonsMethod(F1, F1p, 5, 50));
 
 %2.4 Part 1
 function wk = newtonsMethod(F, Fprime, w0, k)
